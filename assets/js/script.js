5aaf33001231a45b9b9594896354a0c8f40dd5fd
LUIZ HENRIQUE DA CRUZ KIRSCH <0150491>
2024-05-22 19:36:23 -0300

    feat: alteração no style e texto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 const envelope = document.querySelector('.envelope-wrapper');
-        envelope.addEventListener('click', () => {
-        envelope.classList.toggle('flap');
-    });
+const link = document.getElementById('link');
+
+envelope.addEventListener('click', () => {
+  envelope.classList.toggle('flap');
+});
+
+link.addEventListener('click', (event) => {
+  event.stopPropagation();
+});
